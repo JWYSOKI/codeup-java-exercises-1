6c62c3083d977763d7b3f080b08413578b42f787
Chris Allen <chrishughallen@gmail.com>
2018-02-27 16:47:57 -0600

    do while loop exercise 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,12 +1,12 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 0;
+        int i = 100;
 
         do{
             System.out.println(i);
-            i+=2;
+            i-=5;
 
-        } while(i<=100);
+        } while(i>=-10);
 
 
     }
