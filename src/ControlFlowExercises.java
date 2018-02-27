107a2040c0031fc3d8fe54a29c2e109dd49050d9
Chris Allen <chrishughallen@gmail.com>
2018-02-27 16:56:37 -0600

    refactored 100 to -5 loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,12 +1,9 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        double i = 2;
 
-        do{
-            System.out.println((int)i);
-            i = Math.pow(i,2);
-
-        } while(i<1000000);
+        for(int i = 100;i>=-10;i-=5){
+            System.out.println(i);
+        }
 
     }
 }
