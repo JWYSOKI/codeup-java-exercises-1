368d1246dd4e94f5c1bb5b64b1af1300150f0999
Chris Allen <chrishughallen@gmail.com>
2018-02-28 10:16:10 -0600

    updated power table

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,19 +4,29 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
        Scanner scan = new Scanner(System.in);
+boolean confirmation;
+       do {
+           int userInput;
+           int squared;
+           int cubed;
 
-       int userInput;
-       int squared;
-       int cubed;
 
-        System.out.println("Enter an integer");
-        userInput = scan.nextInt();
-        System.out.println("number  |  squared  |  cubed");
-        System.out.println("------  |  -------  |  -----  ");
+           System.out.println("Enter an integer");
+           userInput = scan.nextInt();
+           System.out.println("number  |  squared  |  cubed");
+           System.out.println("------  |  -------  |  -----  ");
 
-        for(int i = 1;i<=userInput;i++){
-            System.out.printf("%-8d" + "| " + "%-11d" + "| " + "%-3d",i,i*i,i*i*i);
-            System.out.println("");
-        }
+           for (int i = 1; i <= userInput; i++) {
+               System.out.printf("%-8d" + "| " + "%-11d" + "| " + "%-3d", i, i * i, i * i * i);
+               System.out.println("");
+           }
+           String keepGoing;
+           System.out.println("Continue - yes/no?");
+           keepGoing = scan.next();
+          confirmation = keepGoing.equals("yes");
+       }
+
+       while(confirmation);
+
         }
     }
