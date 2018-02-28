76c0553ba8b566b8b46e3d2d2bf07649df88efb0
Chris Allen <chrishughallen@gmail.com>
2018-02-28 10:34:47 -0600

    completed all but last exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,29 +4,97 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
        Scanner scan = new Scanner(System.in);
-boolean confirmation;
-       do {
-           int userInput;
-           int squared;
-           int cubed;
+
+//        int i = 5;
+//
+//       while(i<=15){
+//           System.out.print(i + " ");
+//           i++;
+//       }
+//
+
+//        int i = 0;
+//
+//        do{
+//            System.out.println(i);
+//            i+=2;
+//        } while(i<=100);
+
+//        int i = 100;
+//
+//        do{
+//            System.out.println(i);
+//            i-=5;
+//        } while(i>=-10);
+
+//        double i = 2;
+//        do{
+//            System.out.println((int)(i));
+//            i = Math.pow(i,2);
+//        } while(i<100000);
+//
+//        for(int i = 100;i>=-10;i-=5){
+//            System.out.println(i);
+//        }
+//
+//        for (double i = 2;i<100000;i = Math.pow(i,2)){
+//            System.out.println((int)i);
+//        }
+//
+//
+//        for(int i = 1;i<=100;i++){
+//            if(i%15 ==0){
+//                System.out.println("Fizz Buzz");
+//                continue;
+//            }
+//            if(i%3 == 0){
+//                System.out.println("Fizz");
+//                continue;
+//            }
+//            if(i%5 == 0){
+//                System.out.println("Buzz");
+//                continue;
+//            }
+//            else{
+//                System.out.println(i);
+//            }
+//        }
 
 
-           System.out.println("Enter an integer");
-           userInput = scan.nextInt();
-           System.out.println("number  |  squared  |  cubed");
-           System.out.println("------  |  -------  |  -----  ");
 
-           for (int i = 1; i <= userInput; i++) {
-               System.out.printf("%-8d" + "| " + "%-11d" + "| " + "%-3d", i, i * i, i * i * i);
-               System.out.println("");
-           }
-           String keepGoing;
-           System.out.println("Continue - yes/no?");
-           keepGoing = scan.next();
-          confirmation = keepGoing.equals("yes");
-       }
 
-       while(confirmation);
+
+
+//boolean confirmation;
+//       do {
+//           int userInput;
+//           int squared;
+//           int cubed;
+//
+//
+//           System.out.println("Enter an integer");
+//           userInput = scan.nextInt();
+//           System.out.println("number  |  squared  |  cubed");
+//           System.out.println("------  |  -------  |  -----  ");
+//
+//           for (int i = 1; i <= userInput; i++) {
+//               System.out.printf("%-8d" + "| " + "%-11d" + "| " + "%-3d", i, i * i, i * i * i);
+//               System.out.println("");
+//           }
+//           String keepGoing;
+//           System.out.println("Continue - yes/no?");
+//           keepGoing = scan.next();
+//          confirmation = keepGoing.equals("yes");
+//       }
+//
+//       while(confirmation);
+
+
+
+
+
+
+
 
         }
     }
