e706580eb9bdeb16627f0fbb28b3839dd8eebcb7
Chris Allen <chrishughallen@gmail.com>
2018-02-28 13:32:50 -0600

    completed control flow exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -22,50 +22,41 @@ public class ControlFlowExercises {
 
 //        int i = 100;
 //
-//        do{
+//        do {
 //            System.out.println(i);
 //            i-=5;
 //        } while(i>=-10);
-
-//        double i = 2;
+//
+//        long i = 2;
 //        do{
 //            System.out.println((int)(i));
-//            i = Math.pow(i,2);
+//            i *= i;
 //        } while(i<100000);
 //
 //        for(int i = 100;i>=-10;i-=5){
 //            System.out.println(i);
 //        }
 //
-//        for (double i = 2;i<100000;i = Math.pow(i,2)){
-//            System.out.println((int)i);
+//        for (long i = 2;i<100000;i*=i){
+//            System.out.println(i);
 //        }
 //
 //
 //        for(int i = 1;i<=100;i++){
 //            if(i%15 ==0){
 //                System.out.println("Fizz Buzz");
-//                continue;
-//            }
-//            if(i%3 == 0){
+//            } else if(i%3 == 0){
 //                System.out.println("Fizz");
-//                continue;
-//            }
-//            if(i%5 == 0){
+//            } else if(i%5 == 0){
 //                System.out.println("Buzz");
-//                continue;
-//            }
-//            else{
+//            }else{
 //                System.out.println(i);
 //            }
 //        }
-
-
-
-
-
-
+//
 //boolean confirmation;
+//String keepGoing;
+//
 //       do {
 //           int userInput;
 //           int squared;
@@ -78,10 +69,9 @@ public class ControlFlowExercises {
 //           System.out.println("------  |  -------  |  -----  ");
 //
 //           for (int i = 1; i <= userInput; i++) {
-//               System.out.printf("%-8d" + "| " + "%-11d" + "| " + "%-3d", i, i * i, i * i * i);
+//               System.out.printf("%-8d" + "| " + "%-11d" + "| " + "%-1d", i, i * i, i * i * i);
 //               System.out.println("");
 //           }
-//           String keepGoing;
 //           System.out.println("Continue - yes/no?");
 //           keepGoing = scan.next();
 //          confirmation = keepGoing.equals("yes");
@@ -90,41 +80,37 @@ public class ControlFlowExercises {
 //       while(confirmation);
 
 
-boolean confirmation;
-do{
-    int userInput;
-
-    System.out.println("Enter a numerical grade");
-    userInput = scan.nextInt();
-    if(userInput>97){
-        System.out.println("A+");
-    }
-    if(userInput<97 && userInput>93){
-        System.out.println("A");
-    }
-    if(userInput<93 && userInput>=90){
-        System.out.println("A-");
-    }
-    if(userInput<88 && userInput >79){
-        System.out.println("B");
-    }
-    if(userInput<79 && userInput >66){
-        System.out.println("C");
-    }
-    if(userInput<66 && userInput >59){
-        System.out.println("D");
-    }
-    if(userInput<59){
-        System.out.println("F");
-    }
-
-    String keepGoing;
-    System.out.println("Continue - yes/no");
-    keepGoing = scan.next();
-    confirmation = keepGoing.equals("yes");
-
-} while(confirmation);
-
+//boolean confirmation;
+//
+//do{
+//    int userInput;
+////
+//    System.out.println("Enter a numerical grade");
+//    userInput = scan.nextInt();
+//
+//    if(userInput>=90){
+//        System.out.println("A");
+//    }
+//    else if(userInput>=80) {
+//        System.out.println("B");
+//    }
+//    else if(userInput>=70){
+//        System.out.println("C");
+//    }
+//    else if(userInput>= 60){
+//        System.out.println("D");
+//    }
+//    else if(userInput<=50){
+//        System.out.println("F");
+//    }
+//
+//    String keepGoing;
+//    System.out.println("Continue - yes/no");
+//    keepGoing = scan.next();
+//    confirmation = keepGoing.equals("yes");
+//
+//} while(confirmation);
+//
 
         }
     }
---
+++ src/ControlStructureBonuses.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ControlStructureBonuses {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+//        int userInput;
+//        System.out.println("Enter an integer between 1 and 7");
+//        userInput = scan.nextInt();
+//
+//        if(userInput == 1){
+//            System.out.println("Sunday");
+//        }
+//        if(userInput == 2){
+//            System.out.println("Monday");
+//        }
+//        if(userInput == 3){
+//            System.out.println("Tuesday");
+//        }
+//        if(userInput == 4){
+//            System.out.println("Wednesday");
+//        }
+//        if(userInput == 5){
+//            System.out.println("Thursday");
+//        }
+//        if(userInput == 6){
+//            System.out.println("Friday");
+//        }
+//        if(userInput == 7){
+//            System.out.println("Saturday");
+//        }
+
+
+    }
+}
