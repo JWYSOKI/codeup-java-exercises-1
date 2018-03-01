4e5ba6a679b03cb75d9790ec5b62bdde49a13843
Chris Allen <chrishughallen@gmail.com>
2018-03-01 09:04:03 -0600

    completed high low guessing game

--- src/HighLow.java
+++ src/HighLow.java
@@ -2,32 +2,58 @@ import java.util.Scanner;
 
 public class HighLow {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         highLow();
     }
+
+
+    public static void playAgain() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Would you like to play again? [y/n]");
+        String playAgain = scan.nextLine();
+        if(playAgain.equals("y")){
+            highLow();
+        }else{
+            System.out.println("Thanks for playing, better luck next time");
+        }
+    }
+
+
+
+    public static void outOfGuesses(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Really? If you haven't got it by now you never will.");
+        System.out.println("Would you like to play again? [y/n]");
+        String playAgain = scan.nextLine();
+        if(playAgain.equals("y")){
+            highLow();
+        }else{
+            System.out.println("Thanks for playing, better luck next time");
+        }
+    }
+
+
+
     public static void highLow() {
         Scanner scan = new  Scanner(System.in);
         int secretNumber = (int) Math.round(Math.random() * 100);
-        System.out.println(secretNumber);
+//        System.out.println(secretNumber);
         boolean correct = false;
         int count = 0;
             while (!correct) {
                 if(count == 0){
-                    System.out.println("Guess a number between 1 and 100!");
+                    System.out.println("Guess a number between 1 and 100! You get 7 guesses!");
                 }
 
                 if(count ==7){
-                    System.out.println("Come on seriously? If you haven't got it by now you never will. The secret number was " + secretNumber);
-                    System.out.println("Would you like to play again? [y/n]");
-                    String playAgain = scan.nextLine();
-                    if(playAgain.equals("y")){
-                        highLow();
-                    }else{
-                        System.out.println("Thanks for playing, better luck next time");
-                    }
+                    outOfGuesses();
+                    correct = true;
                 }
 
-                if(count >=1){
+                if(count >=1 && count<6){
                     System.out.println("This is guess number " + (count+1) + ", guess again!");
+                }else if (count == 6){
+                    System.out.println("This is your last chance!");
                 }
 
                 int userInput;
@@ -37,49 +63,28 @@ public class HighLow {
                 if(userInput == secretNumber && count == 0){
                     System.out.println("WHOAH! You got it on the first try!");
                     correct = true;
-                    System.out.println("Would you like to play again? [y/n]");
-                    String playAgain = scan.nextLine();
-                    if(playAgain.equals("y")){
-                        highLow();
-                    }else{
-                        System.out.println("Thanks for playing, better luck next time");
-                    }
-                }
+                    playAgain();
 
-                if (userInput == secretNumber && count != 0) {
+                }else if (userInput == secretNumber && count != 0) {
                     count +=1;
-                    System.out.println("You got it, and it only took you " + count + " guesses");
+                    System.out.println("You got it, and it took you " + count + " guesses!");
                     correct = true;
-                    System.out.println("Would you like to play again? [y/n]");
-                    String playAgain = scan.nextLine();
-                    if(playAgain.equals("y")){
-                        highLow();
-                    }else{
-                        System.out.println("Thanks for playing, better luck next time");
-                    }
+                    playAgain();
                 }
-
-
-
-                if (userInput < secretNumber && count<=7) {
+                if(userInput<1 || userInput>100){
+                    System.out.println("That's out of range! Remember, the secret number is between 1 and 100");
+                    count +=1;
+                }else if (userInput < secretNumber && count<=7) {
                     System.out.print("Too low! - ");
                     count +=1;
-
-                }
-
-                if (userInput > secretNumber) {
-                    if(userInput>100){
-                        System.out.println("That's too high. Remember, it's between 1 and 100");
-                        count +=1;
-                    }else {
+                }else if (userInput > secretNumber) {
                         System.out.print("Too high! - ");
                         count += 1;
-                    }
                 }
             }
+        }
     }
 
-}
 
 
 
